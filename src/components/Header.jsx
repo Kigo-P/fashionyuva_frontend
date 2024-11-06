24fb80fea01e91fad0f6a4b35e82a4d7ef0d7165
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-06 15:19:07 +0300

    configured tailwindcss and redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      <Link to="/about-us">About Us</Link>
+    <div className="h-10 w-full bg-red-600 text-red-600">
+      <Link to="/about-us" className="text-red-600">
+        About Us
+      </Link>
     </div>
   )
 }
