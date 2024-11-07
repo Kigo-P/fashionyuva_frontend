72164b4bf66adc37fd5bf27a42cae1f12f09909f
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-07 12:07:38 +0300

    refined listing and landing pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { ShoppingBag, Menu, X } from 'lucide-react'
+import { ShoppingBag, Menu, X, CircleUser, LogOut } from 'lucide-react'
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [profDrop, setProfDrop] = useState(false)
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+  const profDropRef = useRef(null)
+
+  const logged = false
+
+  const handleClickOutside = (event) => {
+    if (profDropRef.current && !profDropRef.current.contains(event.target)) {
+      setProfDrop(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
 
   const navItems = [
     { name: 'Home', href: '/' },
@@ -33,6 +50,59 @@ const Header = () => {
         </nav>
 
         <div className="hidden md:flex items-center space-x-4">
+          <div
+            ref={profDropRef}
+            className="flex items-center justify-center gap-2 relative"
+          >
+            <div
+              className="flex flex-nowrap items-center justify-center gap-2 bg-black p-1 pl-3 cursor-pointer rounded-full"
+              onClick={() => {
+                setProfDrop(!profDrop)
+              }}
+            >
+              <span className="text-white text-xs">Dennis Kibet</span>
+
+              <CircleUser
+                style={{ color: '#fff', fontSize: 18 }}
+                className="cursor-pointer"
+                onClick={() => {
+                  setProfDrop(!profDrop)
+                }}
+              />
+            </div>
+            {profDrop && (
+              <div
+                className="absolute top-8 right-0 bg-white h-auto w-[100px] rounded shadow z-50 overflow-hidden"
+                onClick={(e) => {
+                  e.stopPropagation()
+                }}
+              >
+                <ul>
+                  <Link
+                    href="/profile"
+                    className="p-2 cursor-pointer hover:bg-[#0005] flex items-center justify-start gap-2"
+                  >
+                    <div className="">
+                      <CircleUser style={{ color: '#000', fontSize: 18 }} />
+                    </div>{' '}
+                    <span>Profile</span>
+                  </Link>
+                  <li
+                    className="p-2 cursor-pointer hover:bg-[#0005] flex items-center justify-start gap-2"
+                    onClick={() => {
+                      handleLogout()
+                    }}
+                  >
+                    <div className="">
+                      <LogOut style={{ color: '#000', fontSize: 18 }} />
+                    </div>{' '}
+                    <span>Logout</span>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
           <Link
             to="/login"
             className="border border-slate-200 py-2 px-4 rounded-md cursor-pointer text-white text-xs"
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ShoppingCart, Star } from 'lucide-react'
+
+const ProductCard = ({ product, i }) => {
+  return (
+    <Link to="/">
+      <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white border border-gray-500">
+        <img
+          className="w-full h-48 object-cover"
+          src={`https://picsum.photos/400/300?id=${i + 1}`}
+          alt="Product Image"
+        />
+        <div className="px-6 py-4">
+          <div className="flex justify-between items-start mb-2">
+            <h2 className="font-bold text-xl mb-2">{product.name}</h2>
+          </div>
+          <p className="text-gray-700 text-base mb-4">
+            High-quality wireless headphones with noise cancellation.
+          </p>
+          <div className="flex justify-between items-center mb-4">
+            <span className="text-2xl font-bold text-primary">
+              Ksh {product.price.toLocaleString()}
+            </span>
+            <div className="flex items-center">
+              {[...Array(5)].map((_, i) => (
+                <Star
+                  key={i}
+                  className={`w-5 h-5 ${
+                    i < 4 ? 'text-yellow-400 fill-yellow-400' : 'text-gray-300'
+                  }`}
+                />
+              ))}
+              <span className="text-sm text-gray-600 ml-2">(42 reviews)</span>
+            </div>
+          </div>
+          <button className="w-full p-2 bg-black rounded-md text-white flex items-center justify-center">
+            <ShoppingCart className="mr-2 h-4 w-4" /> <span>Add to Cart</span>
+          </button>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default ProductCard
